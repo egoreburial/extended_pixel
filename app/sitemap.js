2e222ed2e53d7b37762155686b19bd22d93bea33
egore_burial <inblast13@gmail.com>
2024-02-19 16:16:27 +0300

    robots and sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,22 @@
+export default function sitemap() {
+    return [
+        {
+            url: 'https://generalsound.ae',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 1,
+        },
+        {
+            url: 'https://generalsound.ae/en',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 1,
+        },
+        {
+            url: 'https://generalsound.ae/ru',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 1,
+        },
+    ]
+}
